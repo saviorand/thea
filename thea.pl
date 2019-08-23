2e48173eb0a6451ae126939d33db001dab830598
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2019-08-23 12:10:14 +0200

    Cleanup imports (also allows for quick load compilation)

--- thea.pl
+++ thea.pl
@@ -5,10 +5,10 @@
    ;   asserta(user:file_search_path(library, Lib))
    ).
 
-:-use_module('owl2_model.pl').
-:-use_module('owl2_from_rdf.pl').
-:-use_module('owl2_export_rdf.pl').
-:-use_module('owl2_xml.pl').
-:-use_module('owl2_util.pl').
-:-use_module('owl2_io.pl').
+:- use_module(owl2_model).
+:- use_module(owl2_from_rdf).
+:- use_module(owl2_export_rdf).
+:- use_module(owl2_xml).
+:- use_module(owl2_util).
+:- use_module(owl2_io).
 
