65cc979e68d4951cf84f0ab9f6f4a65249653f79
Jan Wielemaker <j.wielemaker@cwi.nl>
2019-09-19 11:53:10 +0200

    ADDED: Started Manchester syntax writer.  Just a stub

---
+++ owl2_export_manchester.pl
@@ -0,0 +1,63 @@
+/* -*- Mode: Prolog -*- */
+
+:- module(owl2_export_manchester,
+          [ owl_generate_manchester/1,          % +FileName
+            owl_generate_manchester/2           % +FileName, +Options
+          ]).
+:- use_module(owl2_model).
+
+/** <module> Generate Manchester syntax
+
+@tbd Almost everything ...
+*/
+
+%!  owl_generate_manchester(+FileName) is det.
+%!  owl_generate_manchester(+FileName, +Options) is det.
+%
+%   Emit the current axioms as Manchester syntax.
+
+owl_generate_manchester(FileName) :-
+    setof(Ontology,ontology(Ontology),[Ontology]),
+    !,
+    owl_generate_manchester(FileName, [ontology(Ontology)]).
+owl_generate_manchester(FileName) :-
+    owl_generate_manchester(FileName, []).
+
+owl_generate_manchester(FileName, Options) :-
+    setup_call_cleanup(
+        open(FileName, write, Out, [encoding(utf8)]),
+        owl_generate_manchester_to_stream(Out, Options),
+        close(Out)).
+
+owl_generate_manchester_to_stream(Out, Options) :-
+    ontology(Ontology, Options),
+    format(Out, 'Ontology: <~w>~n', [Ontology]),
+    save_classes(Out).
+
+ontology(Ontology, Options) :-
+    option(ontology(Ontology), Options),
+    !.
+ontology('http://example.org/ontology#', _).
+
+save_classes(Out) :-
+    forall(axiom(class(Class)),
+           save_class(Out, Class)).
+
+save_class(Out, Class) :-
+    format(Out, '~nClass: ~w~n', [Class]),
+    save_subclasses(Out, Class).
+
+save_subclasses(Out, Class) :-
+    setof(Sub, axiom(subClassOf(Class, Sub)), Subs),
+    !,
+    format(Out, '  SubClassOf:~n', []),
+    save_list(Out, Subs).
+save_subclasses(_, _).
+
+save_list(_Out, []) :- !.
+save_list(Out, [H|T]) :-
+    (   T == []
+    ->  format(Out, '    ~w~n', [H])
+    ;   format(Out, '    ~w,~n', [H]),
+        save_list(Out, T)
+    ).
